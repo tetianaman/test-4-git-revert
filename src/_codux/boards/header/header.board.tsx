5604a06b076afc81e36458d1cfa58c0cef900126
Tetiana Mandziuk <tetianaman@wix.com>
2023-02-06 19:41:50 +0100

    test

--- src/_codux/boards/header/header.board.tsx
+++ src/_codux/boards/header/header.board.tsx
@@ -2,6 +2,6 @@ import { createBoard } from '@wixc3/react-board';
 import { Header } from '../../../header';
 
 export default createBoard({
-    name: 'Header',
+    name: 'Header1',
     Board: () => <Header />
 });
